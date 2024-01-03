25f30f645f09a1529217f231f203e213932a2568
JESSIE T <jessieproductions@aol.com>
2024-01-03 13:35:26 -0800

    testing

--- main.js
+++ main.js
@@ -81,7 +81,7 @@ var downloadNes = document.getElementById('nes')
 downloadNes.addEventListener('click', function(){
     var name = document.getElementById('nesfilename').value;
     spriteRomData = canvasToNES(imageData);
-    download(name || 'sprite.ppf', spriteRomData, 'octect/stream');
+    download(name || 'sprite.chr', spriteRomData, 'octect/stream');
 });
 
 var uploadNes = document.getElementById('nesfile')
@@ -371,12 +371,6 @@ function NEStoCanvas(byteArray){
 /// Translate raw canvas pixles to NES Binary
 
 function canvasToNES(imageData){
-	// PPF Header
-	var PPFData =
-	 {
-	     0x50, 0x50, 0x46, 0x76, 0x01, 0x00, 0x00, 0x00
-	 }; 
-
     // move 16 byte sprite and 512 sprites
     var byteArray = new Uint8Array(512 * 16);
 
@@ -425,9 +419,7 @@ function canvasToNES(imageData){
         xtotal = (xtotal + 8) % width;
     }
 
-    PPFData.push(byteArray);
-
-    return PPFData;
+    return byteArray;
 }
 
 
